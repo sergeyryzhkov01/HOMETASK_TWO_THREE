4603e8d9202c17e6d6bfb6e3b997963c8a601e1f
ryzhk <ryzhk@DESKTOP-DE8A6RJ>
2025-09-29 21:24:32 +0300

    Добавьте файлы проекта.

---
+++ HOMETASK_THREE/main.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h> 
+#include <math.h> 
+
+
+double calculateHypotenuse(double side1, double side2) {
+    
+    return sqrt(side1 * side1 + side2 * side2);
+}
+
+int main() {
+    double side1, side2, hypotenuse;
+
+   
+    printf("Введите длину первой стороны: ");
+    scanf("%lf", &side1); 
+
+    printf("Введите длину второй стороны: ");
+    scanf("%lf", &side2); 
+
+   
+    hypotenuse = calculateHypotenuse(side1, side2);
+
+    
+    printf("Гипотенуза: %.2lf\n", hypotenuse); // Выводим гипотенузу с двумя знаками после запятой
+
+    return 0; 
+}
---
+++ HOMETASK_TWO/main.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include<locale.h>
+int main() {
+    setlocale(LC_ALL, "RUS");
+    double L1, L2, V1, V2;
+
+
+    printf("Введите длину первого поезда (L1): ");
+    scanf("%lf", &L1);
+
+    printf("Введите скорость первого поезда (V1): ");
+    scanf("%lf", &V1);
+
+    printf("Введите длину второго поезда (L2): ");
+    scanf("%lf", &L2);
+
+    printf("Введите скорость второго поезда (V2): ");
+    scanf("%lf", &V2);
+
+
+    if (V1 <= 0 || V2 <= 0) {
+        printf("Ошибка: скорости должны быть положительными.\n");
+        return 1;
+    }
+
+    // Вычисляем относительную скорость
+    double relative_speed = V1 + V2;
+
+    // Вычисляем общее расстояние, которое нужно пройти поездам друг относительно друга
+    double total_distance = L1 + L2;
+
+    // Вычисляем время встречи
+    double time = total_distance / relative_speed;
+
+
+    printf("Время прохождения поездов друг против друга: %.2lf секунд\n", time);
+
+    return 0;
+}
